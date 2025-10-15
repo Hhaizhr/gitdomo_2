fefa7d50f549ae60e8b39f6589cab1a15ba428de
hhaizhr <2568840963@qq.com>
2025-10-15 18:28:42 +0800

    first modify

---
+++ src/main/java/com/hhaizhr/main.java
@@ -0,0 +1,16 @@
+package com.hhaizhr;
+
+/**
+ * ClassName: main
+ * Package: com.hhaizhr
+ * Description:
+ *
+ * @Author Hhaizhr
+ * @Create 2025-10-15 18:27
+ * @Version 1.0
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
